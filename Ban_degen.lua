277e7b5bca1e1cd4a511e78c9356d5147146d0d3
Powderonium <150808874+PowderoniumReal@users.noreply.github.com>
2023-12-23 13:02:39 +0000

    Create Ban_degen.lua
    The script is used to check if the username or display_name of the user has offensive terms or other unwanted ones.

---
+++ Ban_degen.lua
@@ -0,0 +1,36 @@
+local player =game.Players.LocalPlayer
+--Array of banned terms, must be in lowercase
+local prohibited={"55ox95","cmz","bull","goon","bbc","gooning","porn","snowbunny","fuck","sex","snow_bunny","vore","loli","furry","porno","sumiso","submisive"}
+
+function EstaEnArraysimple(arr_allowed, name)
+	for i, value in ipairs(arr_allowed) do
+		if value == name then
+			return true
+		end
+		print()
+	end
+
+	return false
+end
+function isInArray(arr, name)
+	--Array of permitted names, must be in lowercase
+	local allowed={""}
+	for i, value in ipairs(arr) do
+		if string.find(name, value, 1, true) and EstaEnArraysimple(allowed, name)==false then
+			player:Kick("Nombre de usuario inadecuado.")
+
+			return true -- Return true if condition met
+		end
+	end
+	return false -- Return false after checking all elements in arr
+end
+
+local name=player.Name
+local name_display=player.DisplayName
+isInArray(prohibited,string.lower(name))
+isInArray(prohibited,string.lower(name_display))
+
+
+
+
+
